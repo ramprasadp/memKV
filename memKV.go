e2bfa97a45a08151f4ba83521a1f65492d532fe0
Ramprasad Padmanabhan <ramprasad.ap@gmail.com>
2021-01-17 19:11:09 +0530

    working socket

--- memKV.go
+++ memKV.go
@@ -1,12 +1,42 @@
 package main
 
-
 import (
+	"bufio"
 	"fmt"
+	"net"
+	"os"
 )
 
-func main(){
+func openSocket(addr string) net.Listener {
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		fmt.Printf("FATAL: Could not open socket %s \n", err)
+		os.Exit(1)
+	}
+	return ln
 
-	fmt.Printf("Hello from memKV \n")
+}
+
+func processConnection(conn net.Conn) {
+	for {
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		if message == "exit\r\n" {
+			conn.Write([]byte("Ok bye bye\n"))
+			conn.Close()
+			return
+		}
+		fmt.Printf("Message Received: <%s>\n", message)
+	}
+}
+
+func main() {
+
+	// open the socket for communication
+	ln := openSocket(":9980")
+
+	for {
+		conn, _ := ln.Accept()
+		go processConnection(conn)
+	}
 
 }
